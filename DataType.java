9903a8ade22ddddf5942a8e875738f89ea1bc60e
SoftRoad <sao2295@rediffmail.com>
2018-09-27 11:12:36 +0530

    jss

---
+++ DataType.java
@@ -0,0 +1,17 @@
+class DataTypes{
+	public static void main(String []a){
+		int x =85;
+		float k=2.8f;
+		long z=35287895;
+		double d=23.25;
+		short s=56;
+		byte b =98;
+		
+		System.out.println(x);
+		System.out.println(k);
+		System.out.println(z);
+		System.out.println(d);
+		System.out.println(s);
+		System.out.println(b);
+	}
+}
